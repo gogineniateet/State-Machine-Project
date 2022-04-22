31ab0e0f7c88dec5e7d905b988d86a681472d447
AteetGogineni <ateet.gogineni@yesgnome.com>
2022-04-22 16:46:47 +0530

    Player Chase,Attack and death states are updated....

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
-public sealed class GameController 
+public sealed class GameController
 {
     private static GameController instance;
-    public static GameController Instance { 
+    public static GameController Instance
+    {
         get
         {
             if (instance == null)
             {
                 instance = new GameController();
                 instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+                instance.checkpoints = instance.checkpoints.OrderBy(x => x.name).ToList();
             }
             return instance;
         }
-    }
 
+    }
     private List<GameObject> checkpoints = new List<GameObject>();
     public List<GameObject> Checkpoints { get { return checkpoints; } }
    
+
 }
